ccf2fbd5ca889004db35edb12794c42056ffd536
Naveen <naveenn7271@gmail.com>
2024-06-12 04:17:26 +0530

    updates

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 const Photo = () => {
     return (
-        <div className="w-full h-full relative">
+        <div className="w-full h-full relative ">
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{
@@ -29,7 +29,7 @@ const Photo = () => {
                     className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten 
                     absolute">
                     <Image
-                        src="/assets/photo.png"
+                        src="/assets/photo2.png"
                         priority
                         quality={100}
                         fill
